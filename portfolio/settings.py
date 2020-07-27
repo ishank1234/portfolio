dcc7e25d4629957fa0ce980069ead30100a74ba4
Ishank Chopra <49484210+ishank1234@users.noreply.github.com>
2020-07-27 23:38:29 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,7 +137,7 @@ django_heroku.settings(locals())
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ishu.ishank1411@gmail.com'
-EMAIL_HOST_PASSWORD = 'BabU&1424'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
